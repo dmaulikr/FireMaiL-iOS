20a66fdfa5bc27003a0a1f4853b7ba8240684a72
Rafi Rizwan <vi66r@Rafis-MacBook-Pro.local>
2015-04-11 13:55:28 -0400

    Initial Commit

---
+++ FireMaiL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FireMaiL
+//
+//  Created by Rafi Rizwan on 4/11/15.
+//  Copyright (c) 2015 POLYwhirl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FireMaiL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FireMaiL
+//
+//  Created by Rafi Rizwan on 4/11/15.
+//  Copyright (c) 2015 POLYwhirl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
